f541385864076aedd5eec9902d4d31be0b3bdf43
mfadaki <mfadaki@icloud.com>
2022-07-25 01:14:31 +1000

    initial commit

---
+++ inputs/inputs.py
@@ -0,0 +1,28 @@
+alpha = 0.1
+beta = 1
+SL0 = 0.6
+
+n = 100
+mu_t = 100
+sigma_t = 30
+
+ratio_l_nl = 0.5
+
+mu_l = n * ratio_l_nl
+mu_nl = n * ratio_l_nl
+
+sigma_l = sigma_t * 2**(-0.5)
+sigma_nl = sigma_t * 2**(-0.5)
+
+#p_m = 20
+p_m = 10
+#p_r = 10
+p_r = 20
+c_b = 8
+#c_h = 80
+c_h = 80
+#c_h = 200
+#b = 0.5
+
+p_b = 8
+buyback_percent = 0
